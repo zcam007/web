5b93c9beebc00badac9295f368ddf3ac8daf8f29
Chandu Rotta <chandu.rotta@walmart.com>
2025-10-09 14:12:35 -0700

    added tracking

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -15,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <script defer src="http://192.168.4.253:3000/script.js" data-website-id="9f5dcb8e-3ba6-4a8d-a9bb-e160448f64a3"></script>
+        </head>
       <body>{children}</body>
     </html>
   );
